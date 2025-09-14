8662f609832ac38404ca89e9b0c245e4c979dd5b
samuel wagura njuguna <samwagura@gmail.com>
2025-09-14 10:54:17 +0300

    Create 6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+// Create an array of strings
+const languages = [
+    "C is fun",
+    "Python is cool",
+    "JavaScript is amazing"
+];
+
+// Use a loop to print each string in the array
+for (let i = 0; i < languages.length; i++) {
+    console.log(languages[i]);
+}
